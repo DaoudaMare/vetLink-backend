57b5e5660b95d3b4348369b4a18e6759fefddbec
guittiamade <guittiamade04@gmail.com>
2025-03-28 23:53:42 +0000

    importation de la classe profile

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,12 @@
 <?php
 
+use App\Http\Controllers\Api\AuthentificationController;
+use App\Http\Controllers\Api\ProfileProggressController;
+use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\ProduitController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\UserController;
-use App\Http\Controllers\Api\AuthentificationController;
-use App\Http\Controllers\ProduitController;
+
 
 /*
 |--------------------------------------------------------------------------
