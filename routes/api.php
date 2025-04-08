da0f5f032147b28e26db0e76a570607c6eb3687d
Daouda Mare <daoudamare19@gmail.com>
2025-04-08 09:07:35 +0000

    to deploy v1.7

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\User;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
@@ -38,28 +39,42 @@ Route::get('/getUser', function (Request $request) {
 });
 
 
+
 Route::post('/register', function (Request $request) {
     $validator = Validator::make($request->all(), [
         'nom_raison_sociale' => 'required|string|max:255',
         'type_user' => 'required|string|max:255',
-        'secteur_activite' => 'required|string|max:255',
+        'secteur_activite' => 'nullable|string|max:255',
         'email' => 'required|string|email|max:255|unique:users',
-        'telephone' => 'required|string|max:255',
+        'telephone' => 'required|string|max:255|unique:users',
+        'pays' => 'required|string|max:255',
+        'ville' => 'nullable|string|max:255',
+        'coordonnees_gps' => 'nullable|string|max:255',
+        'adresse_physique' => 'nullable|string|max:255',
+        'photo_profil' => 'nullable|string|max:255',
+        'description' => 'nullable|string',
         'liens_reseaux_sociaux' => 'nullable|array',
         'password' => 'required|string|min:8|confirmed',
     ]);
 
     if ($validator->fails()) {
-        return response()->json($validator->errors(), 422);
+        return response()->json(['errors' => $validator->errors()], 422);
     }
 
     $user = User::create([
+        'id' => Str::uuid(),
         'nom_raison_sociale' => $request->nom_raison_sociale,
         'type_user' => $request->type_user,
         'secteur_activite' => $request->secteur_activite,
         'email' => $request->email,
         'telephone' => $request->telephone,
-        'liens_reseaux_sociaux' => json_encode($request->liens_reseaux_sociaux),
+        'pays' => $request->pays,
+        'ville' => $request->ville,
+        'coordonnees_gps' => $request->coordonnees_gps,
+        'adresse_physique' => $request->adresse_physique,
+        'photo_profil' => $request->photo_profil,
+        'description' => $request->description,
+        'liens_reseaux_sociaux' => $request->liens_reseaux_sociaux ? json_encode($request->liens_reseaux_sociaux) : null,
         'password' => Hash::make($request->password),
     ]);
 
