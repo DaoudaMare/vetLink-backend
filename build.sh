95fe03eea3f6c05ce1906b8b6227b7572d3d127c
Daouda Mare <daoudamare19@gmail.com>
2025-04-09 23:38:11 +0000

    deploy v2

--- build.sh
+++ build.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 
-mkdir -p database
-touch database/database.sqlite
-chmod 775 database
-chmod 664 database/database.sqlite
-chown -R www-data:www-data database
-
 # Exécutez les migrations (optionnel, peut aussi être fait au runtime)
 php artisan migrate
 composer install --no-interaction --no-dev --optimize-autoloader
